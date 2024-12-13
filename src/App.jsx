fb00f2760ff9873000eb5bfcdfdb3d93adc2a628
Sinod Kumar <sinodkr001@gmail.com>
2024-12-13 15:33:31 +0530

    fdaaaaaaa

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ChatButton />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/" element={<ChatButton />} /> */}
+        <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </Router>
